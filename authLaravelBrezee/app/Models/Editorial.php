ed23bd6329dcde21c3d93a5be7300eec5c7b33cd
lexm25 <alejandro.martin.alvarez.al@iespoligonosur.org>
2022-02-23 12:24:35 +0100

    clase

--- authLaravelBrezee/app/Models/Editorial.php
+++ authLaravelBrezee/app/Models/Editorial.php
@@ -5,7 +5,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Curso extends Model
+class Editorial extends Model
 {
     use HasFactory;
+    protected $table = "Editorial";
 }
