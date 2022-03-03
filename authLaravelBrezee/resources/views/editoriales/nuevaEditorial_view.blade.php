ca122dd79aed74540025c00a1f4ce833a32a54e8
Lex <alejandromartinalvarez2000@gmail.com>
2022-03-03 19:17:25 +0100

    casa

--- authLaravelBrezee/resources/views/editoriales/nuevaEditorial_view.blade.php
+++ authLaravelBrezee/resources/views/editoriales/nuevaEditorial_view.blade.php
@@ -4,18 +4,18 @@
 <div style="margin-top: 50px"></div>
     <h1 style="text-align: center">NUEVA EDITORIAL</h1>
     <div class="container">
-        <div class="card" style="width: 18rem;margin: auto">
+        <div class="card" style="width: 30%;margin: auto">
             <div class="card-body" >
                 <form action="/editoriales/crearEditorial" method ="POST">
                 <input type="hidden" name="_token" value="csrf_token()">
-                    <label>NOMBRE:</label>
-                    <input type="text" name="nombre" placeholder="Nombre"><br>
+                    <label>NOMBRE:</label><br>
+                    <input class="form-control" type="text" name="nombre" placeholder="Nombre"><br><br>
 
-                    <label>NACIONALIDAD:</label>
-                    <input type="text" name="nacionalidad" placeholder="Nacionalidad">
+                    <label>NACIONALIDAD:</label><br>
+                    <input class="form-control" type="text" name="nacionalidad" placeholder="Nacionalidad"><br><br>
 
-                    <label>id</label>
-                    <input type="number" placeholder="Introduce el id de la Editorial" name="id" maxlength="11"><br>
+                    <label>id</label><br>
+                    <input class="form-control" type="number" placeholder="Introduce el id de la Editorial" name="id" maxlength="11"><br>
                     <br>
                     <div class="d-grid gap-2 col-6 mx-auto">
                         <input class="btn btn-secondary" type="submit" value="Guardar">
--- authLaravelBrezee/resources/views/libros/nuevoLibro_view.blade.php
+++ authLaravelBrezee/resources/views/libros/nuevoLibro_view.blade.php
@@ -4,28 +4,28 @@
 <div style="margin-top: 50px"></div>
     <h1 style="text-align: center">NUEVO LIBRO</h1>
     <div class="container">
-        <div class="card" style="width: 18rem;margin: auto">
+        <div class="card" style="width: 30%;margin: auto">
             <div class="card-body" >
                 <form action="/libros/crearLibro" method ="POST">
                     <input type="hidden" name="_token" value="csrf_token()">
 
-                    <label>ISBN:</label>
-                    <input type="text" name="ISBN" maxlength="13" placeholder="ISBN (13 carácteres)"><br>
-                    
-                    <label>TITULO:</label>
-                    <input type="text" name="titulo" placeholder="Título"><br>
-                
-                    <label>AUTOR:</label>
-                    <input type="text" name="autor" placeholder="Autor/a"><br>
-                
-                    <label>IDIOMA:</label>
-                    <input type="text" name="idioma" placeholder="Idioma">
-                    
+                    <label>ISBN:</label><br>
+                    <input class="form-control" type="text" name="ISBN" maxlength="13" placeholder="ISBN (13 carácteres)"><br>
+                    <br>
+                    <label>TITULO:</label><br>
+                    <input class="form-control" type="text" name="titulo" placeholder="Título"><br>
+                    <br>
+                    <label>AUTOR:</label><br>
+                    <input class="form-control" type="text" name="autor" placeholder="Autor/a"><br>
+                    <br>
+                    <label>IDIOMA:</label><br>
+                    <input class="form-control" type="text" name="idioma" placeholder="Idioma">
+                    <br>
                     <br><label>PUBLICACIÓN</label><br>
-                    <input type="date" name="publicacion"><br>
-                    
+                    <input class="form-control" type="date" name="publicacion"><br>
+                    <br>
                     <label>EDITORIAL</label>
-                    <input type="number" placeholder="Introduce el id de la Editorial" name="editorial"><br><br>
+                    <input class="form-control" type="number" placeholder="Introduce el id de la Editorial" name="editorial"><br><br>
                     <div class="d-grid gap-2 col-6 mx-auto">
                         
                         <input class="btn btn-secondary" type="submit" value="Guardar">
